8b0096e61092478b19ffa104b6e408f793c6e0ef
Caro2018 <caliagav1058@gmail.com>
2019-11-09 10:34:24 -0500

    spring

---
+++ classuni/src/main/java/pe/edu/uni/fiis/classuni/ClassuniApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.fiis.classuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassuniApplication.class, args);
+	}
+
+}
