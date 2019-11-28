8f6642418e6dea74943a8497fe1a20009fa5339e
ingenieria <patrick.h291299@gmail.com>
2019-11-28 11:57:30 -0500

    1rafuncion

--- classuniweb/src/main/resources/static/assets/js/app.js
+++ classuniweb/src/main/resources/static/assets/js/app.js
@@ -1,2 +1,44 @@
-new Vue
+new Vue({
+    el: '#controlador',
+    data:{
+        nombre:'',
+        apellido:'',
+        dni:'',
+        correo:'',
+        id:'',
+        codigo_uni :'',
+        clave:'',
+        confirmacion:null
+    },
+    methods:{
+        crear : function () {
+            var vm = this;
+            var url = 'http://localhost:8080/RegistrarUsuario';
+            var data = {
+                "nombreUsuario" : this.nombre,
+                "apellidoUsuario": this.apellido,
+                "dni": this.dni,
+                "correo":this.correo,
+                "codUsuario":this.codigo_uni,
+                "password":this.clave
+
+            };
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+                console.log('Success:', response);
+            this.confirmacion='ingreso correcto';
+            alert(this.confirmacion);
+        })
+        }
+
+    }
+
+});
 
