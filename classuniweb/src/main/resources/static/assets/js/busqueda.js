3a9aa845c9e37f35ed3ed105c586c94737197f2d
ingenieria <patrick.h291299@gmail.com>
2019-11-30 11:16:04 -0500

    485

--- classuniweb/src/main/resources/static/assets/js/busqueda.js
+++ classuniweb/src/main/resources/static/assets/js/busqueda.js
@@ -1,3 +1,24 @@
 new Vue({
-    el:''
-})
+    el:'#busqueda',
+    data:{
+        ambiente:
+            [
+                {
+                    codigo:'123',
+                    tipo:'125'
+                },
+                {
+                    codigo:'123',
+                    tipo:'1215'
+                },
+                {
+                    codigo:'1478',
+                    tipo:'100'
+                }
+            ]
+
+    },
+    methods:{
+
+    }
+});
