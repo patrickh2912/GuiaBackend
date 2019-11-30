0226fae87c4ef75c9d78c0c336ec158ab4205cc8
ingenieria <patrick.h291299@gmail.com>
2019-11-30 06:53:34 -0500

    funciones

--- classuniweb/src/main/resources/static/assets/js/app.js
+++ classuniweb/src/main/resources/static/assets/js/app.js
@@ -1,29 +1,93 @@
 new Vue({
     el: '#controlador',
-    data:{
-        nombre:'',
-        apellido:'',
-        dni:'',
-        correo:'',
-        id:'',
-        codigo_uni :'',
-        clave:'',
-        confirmacion:null
+    data: {
+        nombre: '',
+        apellido: '',
+        dni: '',
+        correo: '',
+        id: '',
+        codigo_uni: '',
+        clave: '',
+        tipo: 'Alumno',
+        condicion: 'Activo',
+        confirmacion: null
     },
-    methods:{
-        crear : function () {
+    methods: {
+        crear: function () {
             var vm = this;
             var url = 'http://localhost:8080/RegistrarUsuario';
             var data = {
-                "nombreUsuario" : this.nombre,
+                "nombreUsuario": this.nombre,
                 "apellidoUsuario": this.apellido,
                 "dni": this.dni,
-                "correo":this.correo,
-                "codUsuario":this.codigo_uni,
-                "password":this.clave
-
+                "correo": this.correo,
+                "codUsuario": this.codigo_uni,
+                "password": this.clave,
+                "tipoUsuario": this.tipo,
+                "condicion": this.condicion,
+                "id_usuario": this.id
             };
             fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res = > res.json()
+        ).catch(error = > console.error('Error:', error)
+        ).then(response = > {
+                console.log('Success:', response);
+            this.confirmacion = 'ingreso correcto';
+            this.asignar_id();
+            alert(this.confirmacion + " " + this.id + " " + this.clave);
+        })
+        },
+        asignar_id: function () {
+            var vm = this;
+            var url = 'http://localhost:8080/generarIdUsuario';
+            var data = {
+                "nombreUsuario": this.nombre,
+                "apellidoUsuario": this.apellido,
+                "dni": this.dni,
+                "correo": this.correo,
+                "codUsuario": this.codigo_uni,
+                "password": this.clave,
+                "tipoUsuario": this.tipo,
+                "condicion": this.condicion,
+                "id_usuario": this.id
+            };
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res = > res.json()
+        ).catch(error = > console.error('Error:', error)
+        ).then(response = > {
+                console.log('Success:', response);
+            vm.crear();
+        })
+        }
+    }
+});
+
+new Vue({
+    el:'#acceso',
+    data :{
+        username:'',
+        pass:'',
+        ventana:'log_in'
+    },
+    methods:{
+        login :function () {
+            var vm =this;
+            var url ='http://localhost:8080/generarIdUsuario';
+            var data ={
+                "id_usuario":this.username,
+                "password":this.pass
+            };
+            fetch(url,{
                 method: 'POST',
                 body: JSON.stringify(data),
                 headers:{
@@ -33,12 +97,8 @@ new Vue({
         .catch(error => console.error('Error:', error))
         .then(response => {
                 console.log('Success:', response);
-            this.confirmacion='ingreso correcto';
-            alert(this.confirmacion);
+                this.ventana='home';
         })
         }
-
     }
-
-});
-
+})
