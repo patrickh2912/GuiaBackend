540b4433b82d8ac7ed7370a64380485894ee84e5
Caro2018 <caliagav1058@gmail.com>
2019-11-30 18:54:38 -0500

    asignar listas a select

--- classuniweb/src/main/resources/static/assets/js/app.js
+++ classuniweb/src/main/resources/static/assets/js/app.js
@@ -1,50 +1,87 @@
-
-
 new Vue({
-    el:'#acceso',
-    data :{
-        username:'',
-        pass:'',
-        ventana:'log',
-        cod: 'S4-105'
-    },
-    methods:{
-        login :function () {
-            var vm =this;
-            var url ='http://localhost:8080/login';
-            var data ={
-                "idUsuario":this.username,
-                "password":this.pass
-            };
-            fetch(url,{
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers:{
-                    'Content-Type': 'application/json'
-                }
-            }).then(response => {
-                console.log('Success:', response);
-            vm.ventana='busqueda';
-            console.log(this.username + this.pass)
-            })
-        .catch(error => console.error('Error:', error))
+    el: '#acceso',
+    data: {
+        username: '',
+        pass: '',
+        ventana: 'log',
+        cod: 'S4-105',
+        tipoAmbiente:'',
+        ambienteList: null,
+        ambienteHorario: null,
+        horarioList: null
 
-        },
-        traerHorario : function () {
+    },
+    methods: {
+        login: function () {
             var vm = this;
-            var url = 'http://localhost:8080/ambienteHorario';
+            var url = 'http://localhost:8080/login';
             var data = {
-                "codAmbiente": vm.cod
+                "idUsuario": this.username,
+                "password": this.pass
             };
-                        fetch(url, {
+                        fetch(url,{
                             method: 'POST',
                             body: JSON.stringify(data),
                             headers:{
                                 'Content-Type': 'application/json'
                             }
-                        }).then(response => {
-                            vm.ambiente = response.lista
-                    }).catch(error => console.error('Error:', error))
+                        }).then(res => res.json()).catch(error => console.error('Error:', error))
+                    .then(response => {
+                            console.log(response);
+                            if(response.usuario != null){
+                                vm.ventana='busqueda';
+                            }
+                        })
+                    .catch(error => console.error('Error:', error))
+
+        },
+        traerHorario: function () {
+            var vm = this;
+            var url = 'https://classuni.getsandbox.com/ambientehorario';
+            var data = {
+                "codAmbiente": vm.cod
+            };
+                                  fetch(url, {
+                                        method: 'POST',
+                                        body: JSON.stringify(data),
+                                        headers:{
+                                            'Content-Type': 'application/json'
+                                        }
+                                    }).then(res => res.json())
+                                        .catch(error => console.error('Error:', error))
+                                        .then(response => {vm.ambiente = response.lista;})
+        },
+        buscarAmbiente: function () {
+            var vm = this;
+            var url = 'https://classuni.getsandbox.com/ambientehorario';
+            var data = {
+                "codAmbiente": vm.tipoAmbiente
+            };
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {vm.ambienteList = response;})
+        },
+        buscarHorario: function () {
+            var vm = this;
+            var url = 'https://classuni.getsandbox.com/ambientehorario';
+            var data = {
+                "codAmbiente": vm.ambienteHorario
+            };
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {vm.horarioList = response;})
         }
     }
 });
@@ -81,7 +118,7 @@ new Vue({
     methods: {
         crear: function () {
             var vm = this;
-            var url = 'http://localhost:8080/RegistrarUsuario';
+            var url = 'RegistrarUsuario';
             var data = {
                 "nombreUsuario": this.nombre,
                 "apellidoUsuario": this.apellido,
@@ -99,15 +136,12 @@ new Vue({
                             headers: {
                                 'Content-Type': 'application/json'
                             }
-                        }).then(response => {
+                        }).then(res => res.json())
+                        .catch(error => console.error('Error:', error))
+                            .then(response => {
                         vm.confirmacion = 'ingreso correcto';
                         alert(this.confirmacion + " " + this.id + " " + this.clave);
                     })
-        },
-
-
-
-
-
+        }
     }
 });
