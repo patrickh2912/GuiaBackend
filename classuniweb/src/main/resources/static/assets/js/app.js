860536de1ce970baf251bb2f94db0490d9400220
ingenieria <patrick.h291299@gmail.com>
2019-12-01 22:46:57 -0500

    s

--- classuniweb/src/main/resources/static/assets/js/app.js
+++ classuniweb/src/main/resources/static/assets/js/app.js
@@ -106,8 +106,7 @@ new Vue({
                     'Content-Type': 'application/json'
                 }
             }).then(res => res.json()).catch(error => console.error('Error:', error))
-        .then(response => {vm.ambienteList = response.lista;
-        vm.mostrarPedido();})
+        .then(response => {vm.ambienteList = response.lista;})
         },
  ////////////////////////////////////////////////////////////////////////////////////////////////////////
         buscarHorario: function () {
